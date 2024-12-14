1c15e2a4bb8e0e61e64706910caf4482d391542f
leandroffdev <leandrofrancagti@gmail.com>
2024-12-14 17:14:15 -0300

    commit final

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -1,18 +1,11 @@
 import { Login } from './login';
 
-import {describe, expect, test} from '@jest/globals';
+import {describe, expect} from '@jest/globals';
 
 describe('login', () => {
-    function soma(a:number, b:number): number {
-        return a + b;
-    }
     it('Deve retornar a mensagem de boas vindas', () => {
         const result = Login('email', 'senha'); // Chama a função
         expect(result).toHaveBeenCalledWith(/Bem vindo(a)/i); // Verifica o retorno
-    });
-
-    test('adds 1 + 2 to equal 3', () => {
-        expect(soma(1, 2)).toEqual(3);
     });
 });
 
